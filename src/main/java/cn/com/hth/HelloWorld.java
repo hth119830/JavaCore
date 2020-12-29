ed68e6025bc13950b025c0a497b3fec2a3994555
hth119830 <1729135278@qq.com>
2020-12-29 22:39:06 +0800

    aa

--- src/main/java/cn/com/hth/HelloWorld.java
+++ src/main/java/cn/com/hth/HelloWorld.java
@@ -8,4 +8,9 @@ package cn.com.hth;
  * @date 2020/12/29 21:25
  */
 public class HelloWorld {
+    //    unicode 码 \u
+    public static void main(String[] args) {
+        System.out.println("\u03C0");
+    }
+
 }
